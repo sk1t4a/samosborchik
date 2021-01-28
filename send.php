2834dbe74befc5a469ae5d501d0bd014c9004396
sk1t4a <45572083+sk1t4a@users.noreply.github.com>
2021-01-29 01:42:32 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,6 @@
+$name = $_POST['name'];
+$textarea = $_POST['textarea'];
+$email = $_POST['email'];
+$name = htmlspecialchars($name);
+$textarea = htmlspecialchars($textarea);
+$email = htmlspecialchars($email);
